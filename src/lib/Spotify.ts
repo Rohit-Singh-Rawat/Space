ecb6f42c30dc8e088cad62b8c4025a3306be8143
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-08-09 06:56:44 +0530

    Retrying fixing error

--- src/lib/Spotify.ts
+++ src/lib/Spotify.ts
@@ -63,44 +63,39 @@ export interface NowPlayingData {
 }
 
 export const getNowPlaying = async (): Promise<NowPlayingData | string> => {
-  try {
-    const { access_token } = await getAccessToken()
-    const response = await fetch(NOW_PLAYING_ENDPOINT, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-      next: { revalidate: 1 },
-    })
+  const { access_token } = await getAccessToken()
+  const response = await fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    cache: 'no-cache',
+  })
 
-    if (response.status > 400) {
-      throw new Error('Unable to Fetch Song')
-    } else if (response.status === 204) {
-      throw new Error('Currently Not Playing')
-    }
-    const song: SpotifySong = await response.json()
+  if (response.status > 400) {
+    throw new Error('Unable to Fetch Song')
+  } else if (response.status === 204) {
+    throw new Error('Currently Not Playing')
+  }
+  const song: SpotifySong = await response.json()
 
-    const albumImageUrl = song.item.album.images[0].url
-    const artist = song.item.artists.map((artist) => artist.name).join(', ')
-    const isPlaying = song.is_playing
-    const songUrl = song.item.external_urls.spotify
-    const title = song.item.name
-    const timePlayed = song.progress_ms
-    const timeTotal = song.item.duration_ms
-    const artistUrl = song.item.album.artists[0].external_urls.spotify
+  const albumImageUrl = song.item.album.images[0].url
+  const artist = song.item.artists.map((artist) => artist.name).join(', ')
+  const isPlaying = song.is_playing
+  const songUrl = song.item.external_urls.spotify
+  const title = song.item.name
+  const timePlayed = song.progress_ms
+  const timeTotal = song.item.duration_ms
+  const artistUrl = song.item.album.artists[0].external_urls.spotify
 
-    return {
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title,
-      timePlayed,
-      timeTotal,
-      artistUrl,
-    }
-  } catch (error) {
-    console.error('Error fetching currently playing song: ', error)
-    return error instanceof Error ? error.message : 'An unknown error occurred'
+  return {
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+    timePlayed,
+    timeTotal,
+    artistUrl,
   }
 }
 
