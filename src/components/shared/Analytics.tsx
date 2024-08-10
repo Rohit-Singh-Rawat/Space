7b0764bbaf2d970f04f72a4c8e40b20699d8dcf8
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-08-11 00:03:14 +0530

    Fixing analytics

--- src/components/shared/Analytics.tsx
+++ src/components/shared/Analytics.tsx
@@ -2,6 +2,11 @@ import Script from 'next/script'
 import React from 'react'
 
 export const Analytics = () => {
+  console.log(
+    process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+    process.env.NEXT_PUBLIC_UMAMI_URL,
+    process.env.NODE_ENV
+  )
   return (
     <>
       {process.env.NODE_ENV === 'production' && (
